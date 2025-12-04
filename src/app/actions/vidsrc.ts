fab3dba5432c27cb1c902d4b65a2e8ea0adfd046
Null_v1x <juanmakate0517@gmail.com>
2025-12-04 10:56:19 -0500

    fix: remove broken vidsrc import from actions

--- src/app/actions/vidsrc.ts
+++ src/app/actions/vidsrc.ts
@@ -1,24 +1,6 @@
 'use server';
 
-import { VIDSRC_BASE_URL } from '@/services/vidsrc';
-
 export async function checkVidsrcAvailability(imdbId: string, type: 'movie' | 'tv', season: number = 1, episode: number = 1): Promise<boolean> {
-    try {
-        let url = '';
-        if (type === 'movie') {
-            url = `${VIDSRC_BASE_URL}/embed/movie/${imdbId}`;
-        } else {
-            url = `${VIDSRC_BASE_URL}/embed/tv/${imdbId}/${season}/${episode}`;
-        }
-
-        const response = await fetch(url, {
-            method: 'HEAD',
-            cache: 'no-store'
-        });
-
-        return response.status === 200;
-    } catch (error) {
-        console.error('Error checking vidsrc availability:', error);
-        return false;
-    }
+    // Streaming functionality has been removed
+    return false;
 }
